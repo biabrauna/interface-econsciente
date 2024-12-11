83855023c31dfdc8af38786cfd3c221283850523
Miguel <migcunhanogueira@gmail.com>
2024-12-11 07:04:02 -0300

    feat: create interface-econsciente

--- src/routes/SplashScreen.jsx
+++ src/routes/SplashScreen.jsx
@@ -12,7 +12,7 @@ export default function SplashScreen() {
     })
 
     return (
-        <div style={{ backgroundImage: "url(https://console.cloudinary.com/pm/c-d2ad94b7829cc5889bcd8db4d261c9/media-explorer?assetId=702650c913e35e4b6b3d4fe503112a1f)", display: "flex", justifyContent: "center", alignItems: "center", height: "100%", width: "100%" }}>
+        <div style={{ backgroundImage: `url('https://res.cloudinary.com/dnulz0tix/image/upload/v1733910597/mc8vbxz0qmdqgr1e2bnn.png')`, display: "flex", justifyContent: "center", alignItems: "center", height: "100%", width: "100%" }}>
             <div className="App">
             <img src="https://res.cloudinary.com/dnulz0tix/image/upload/v1733910597/z1yhunaucajmbcydnexn.png" className="App-logo" alt="logo" />
             </div>
